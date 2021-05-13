029470d48c89791a4dcebb444a165435150e45ca
Pururaj Solanki <batman@Pururajs-MBP.austin.rr.com>
2021-05-13 18:35:44 -0500

    finished my recipe project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Recipie from "./components/Recipie";
+import Empty from "./components/Empty";
 
 function App() {
+  const [recipies, setRecipies] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('banana');
+  const banana = "banana"
+
+
+  useEffect(() => {
+    getRecipies();
+  }, [query]);
+
+  const APP_ID = "a58eec9b";
+  const APP_KEY = "ece302170c08adf47541f1352923f55d";
+
+  const getRecipies = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipies(data.hits);
+    console.log(data.hits);
+  }
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="logo">
+          <h1>Recipe Log Book</h1>
+        </div>
+        <div className="bar-btn">
+          <input className="search-bar" type="text" value={search} onChange={handleChange} autoFocus placeholder="Enter Recipe" />
+          <button className="search-button" type="submit">Search</button>
+        </div>
+
+      </form>
+      <div className="recipies">
+        {recipies.length !== 0
+          ?
+          recipies.map((recipe) => (
+            <Recipie key={recipe.recipe.calories} name={recipe.recipe.label} ingredients={recipe.recipe.ingredients} type={recipe.recipe.dishType} cusine={recipe.recipe.cuisineType} calories={recipe.recipe.calories} image={recipe.recipe.image} />
+          ))
+          :
+          <Empty />
+        }
+
+      </div>
     </div>
   );
 }
